f2ca07d9be60d9ee742a4b1f568eb007f81456d2
aaron <kopplina@mail.gvsu.edu>
2023-12-07 21:03:03 -0500

    fixing syntax issues with await / object desctructuring

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-import {config} from 'dotenv';
+import { config } from "dotenv";
 
 export function loadConfig() {
-    config();
-    return {
-        lnd: {
-            cert: process.env.LND_TLS_CERT_B64,
-            macaroon: process.env.LND_MACAROON_B64,
-            socket: process.env.LND_ADDRESS,
-        }
-    }
+  config();
+  return {
+    lnd: {
+      cert: process.env.LND_TLS_CERT_B64,
+      macaroon: process.env.LND_MACAROON_B64,
+      socket: process.env.LND_ADDRESS,
+    },
+  };
 }
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-import lnService from 'ln-service';
-import * as db from './db.js';
-import * as lnd from './lnd.js';
-import {loadConfig} from './config.js';
+import lnService from "ln-service";
+import * as db from "./db.js";
+import * as lnd from "./lnd.js";
+import { loadConfig } from "./config.js";
 
 async function main() {
-    const cfg = loadConfig();
-    const {lndClient} = lnd.connect(cfg.lnd);
-    
-    decode_invoice(lndClient, "lnbcrt1u1pjkk7gmpp55g6pydqjxsfrgy35zg6pydqjxsfrgy35zg6pydqjxsfrgy35zg6qdqqcqzzsxqyz5vqsp5j8zq3wylfp5pzqsndy3ylqphnuajeyfctxwwule2d82nqmf8r87q9qyyssq0ymq992gg0zfdknghp54wxef3svkxwxk0l4an805d4mf3w69ma95ekmunnd26e2zenvthjthhs342x9z0z372e9xpwvaqlgm9uqu3dsp6kdd4u");
-    let invoice = add_hold_invoice(lndClient, 100, "a234123412341234123412341234123412341234123412341234123412341234")
+  const cfg = loadConfig();
+  const client = lnd.connect(cfg.lnd);
+
+  const res = await lnd.decode_invoice(
+    client,
+    "lnbc200u1pjhy7p5pp5tlgw7xkccvk5vjmtmlwm8urvfccmn827drz4kt624a3qtmn38lqqdr62pskjepqw3hjqsnfw33k76twv4ezq3njv4jkcctwvdjjq2z0wfjx2u3qf9zr5gpsx5mnwwfexqcj6etyvenz6drrxesj6wryvvcz6dp3vyuxvd3hx56nqdmy9ycqzzsxqrrs0sp5pxhcevcfmh7gnazaxy2fxh2fn24s7my2jzszy4n50vsd053mzs8s9qyyssqwu0u54wgcla07d8dxc30e3e5ng75c29t50n0jgtdy9tyf9c0wwlxkwpqdlzxvvggzy59xd5xnlswquskuakcegf0r03mghmvs3s0zvgqyszval"
+  );
+  let invoice = lnd.add_hold_invoice(
+    client,
+    100,
+    "a234123412341234123412341234123412341234123412341234123412341234"
+  );
 }
 
 async function handle_customer_invoice(lndClient, invoice) {
-    let wrapped_invoice = await wrap_invoice(lndClient, invoice);
-    db.saveInvoiceToDB(invoice);
-    return res;
+  let wrapped_invoice = await lnd.wrap_invoice(lndClient, invoice);
+  db.saveInvoiceToDB(invoice);
+  return res;
 }
 
 main();
